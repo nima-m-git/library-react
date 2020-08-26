253c27c0ded1a376c690b687b1aac98d660b35ab
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-08-25 21:09:31 -0400

    Updates

---
+++ precache-manifest.ecae7f79a1446fcf597b8c1acf3ab22b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b356414522d4c77a5d324bafa2af6f",
+    "url": "/library-react/index.html"
+  },
+  {
+    "revision": "dcda674f6dfcb18939e4",
+    "url": "/library-react/static/css/main.c8689fe6.chunk.css"
+  },
+  {
+    "revision": "ca875737870defbf9fb0",
+    "url": "/library-react/static/js/2.47414c62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/library-react/static/js/2.47414c62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcda674f6dfcb18939e4",
+    "url": "/library-react/static/js/main.a6b66ca3.chunk.js"
+  },
+  {
+    "revision": "080c0194f9f051978511",
+    "url": "/library-react/static/js/runtime-main.ce914d91.js"
+  },
+  {
+    "revision": "cb394bc0430cab57f3aba97bdcb35b08",
+    "url": "/library-react/static/media/Magnificent Personal Use.cb394bc0.ttf"
+  },
+  {
+    "revision": "8a675039f4e58b1ed7892109dbc59e66",
+    "url": "/library-react/static/media/library-img.8a675039.png"
+  }
+]);
